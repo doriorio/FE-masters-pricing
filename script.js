8553b6251a3124d4f8cdededa509209c230af8c3
doriorio <itsdori@gmail.com>
2020-06-28 10:26:37 -0700

    fixed some styles

--- script.js
+++ script.js
@@ -6,28 +6,21 @@ var yearlyPricing = document.getElementsByClassName('pricing-yearly');
 checkbox.addEventListener('change', function () {
   if (checkbox.checked) {
     Array.prototype.forEach.call(monthlyPricing, price => {
-      if (price.classList.contains('pricing-off')){
         price.classList.remove('pricing-off');
-      }
+
     });
     Array.prototype.forEach.call(yearlyPricing, price => {
-      if (!price.classList.contains('pricing-off')){
         price.classList.add('pricing-off');
-      }
     });
 
   } else {
     //Not checked = yearly
     // do that
     Array.prototype.forEach.call(yearlyPricing, price => {
-      if (price.classList.contains('pricing-off')){
         price.classList.remove('pricing-off');
-      }
     });
     Array.prototype.forEach.call(monthlyPricing, price => {
-      if (!price.classList.contains('pricing-off')){
         price.classList.add('pricing-off');
-      }
     });
   }
 });
